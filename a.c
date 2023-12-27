e5b48e58b539e3c8e90bff487f5fe9b73cb8093a
NithinYesudas <nithinypadickal@gmail.com>
2023-12-27 15:50:57 +0530

    completed all llex programs

---
+++ a.c
@@ -0,0 +1,9 @@
+int main()
+{
+    int a = 0;
+    int baf = 0;
+    if ( a == baf)
+    {
+        return 0;
+    } 
+}
---
+++ output.c
@@ -0,0 +1,9 @@
+int main()
+begin
+    int a = 0;
+    int baf = 0;
+    if ( a == baf)
+    begin
+        return 0;
+    end 
+end
